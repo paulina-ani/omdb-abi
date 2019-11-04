c44e2fb13759a5f09adb7577688c2883338dd5c3
Paulina <aniskiewicz.paulina@gmail.com>
2019-11-04 18:58:06 +0100

    showDetails function added

--- index.js
+++ index.js
@@ -33,6 +33,74 @@ const createList = data => {
   }
 };
 
+const showDetails = details => {
+  var listItemTitle = document.createElement("ul");
+  listItemTitle.classList.add("movieTitle");
+  listItem.appendChild(listItemTitle);
+  listItemTitle.innerText = `Title: ${details.Title}`;
+
+  const listItemReleased = document.createElement("li");
+  listItemReleased.classList.add("movieReleased");
+  listItemTitle.appendChild(listItemReleased);
+  if (details.Released !== "N/A") {
+    listItemReleased.innerText = details.Released;
+  } else {
+    listItemReleased.innerText = "No data available";
+  }
+
+  const listItemRated = document.createElement("li");
+  listItemRated.classList.add("movieRated");
+  listItemTitle.appendChild(listItemRated);
+  if (details.Rated !== "N/A") {
+    listItemRated.innerText = `Rating: ${details.imdbRating}`;
+  } else {
+    listItemRated.innerText = "Rating: No data available";
+  }
+
+  const listItemRuntime = document.createElement("li");
+  listItemTitle.appendChild(listItemRuntime);
+  if (details.Runtime !== "N/A") {
+    listItemRuntime.innerText = `Runtime: ${details.Runtime}`;
+  } else {
+    listItemRuntime.innerText = "Runtime: No data available";
+  }
+
+  const listItemDescription = document.createElement("li");
+  listItemTitle.appendChild(listItemDescription);
+
+  var myDetails = details.Plot;
+  var maxLength = 100;
+  var shortenedDetails = myDetails.substr(0, maxLength);
+  shortenedDetails = shortenedDetails.substr(
+    0,
+    Math.min(shortenedDetails.length, shortenedDetails.lastIndexOf(" "))
+  );
+
+  if (details.Plot !== "N/A") {
+    listItemDescription.innerText = `Description: ${shortenedDetails}`;
+  } else {
+    listItemDescription.innerText = "Description: No data available";
+  }
+
+  const listItemAwards = document.createElement("li");
+  listItemTitle.appendChild(listItemAwards);
+  if (details.Awards !== "N/A") {
+    listItemAwards.innerText = `Awards: ${details.Awards}`;
+  } else {
+    listItemAwards.innerText = "Awards: No data available";
+  }
+
+  var imageCreate = document.createElement("img");
+  imageCreate.src = details.Poster;
+
+  if (details.Poster !== "N/A") {
+    listItemTitle.appendChild(imageCreate);
+  } else {
+    imageCreate.src = "/pictures/default.png";
+    listItemTitle.appendChild(imageCreate);
+  }
+};
+
 const submitApp = function() {
   var titleSearch = document.getElementById("inputText").nodeValue;
   var urlSearch = "http://www.omdbapi.com/?apikey=f4f6588c&s=" + titleSearch;
