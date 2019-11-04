b53f0473697b08c2ba2aac16a3b4076ede1d346b
Paulina <aniskiewicz.paulina@gmail.com>
2019-11-04 17:41:02 +0100

    submitApp function added

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ const requestData = (url, handler) => {
     })
     .then(handler);
 };
+
+const submitApp = function() {
+  var titleSearch = document.getElementById("inputText").nodeValue;
+  var urlSearch = "http://www.omdbapi.com/?apikey=f4f6588c&s=" + titleSearch;
+  fetch(urlSearch)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Something is wrong with the data. Please check");
+    })
+    .then(data => {
+      return (data.totalResults / 10).toFixed();
+    })
+    .then(numberOfResults => {
+      for (var i = 0; i < numberOfResults; i++) {
+        var urlPages =
+          "http://www.omdbapi.com/?apikey=f4f6588c&s=" +
+          titleSearch +
+          "&page=" +
+          [i + 1];
+        requestData(urlPages, createList);
+      }
+    });
+};
