e9cbf0fe39cd27ee98623df2db5b0e0d5a6fbb1b
Paulina <aniskiewicz.paulina@gmail.com>
2019-11-04 18:56:46 +0100

    createList function implemented

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const listItem = document.getElementById("list");
+
 const requestData = (url, handler) => {
   fetch(url)
     .then(response => {
@@ -7,6 +9,28 @@ const requestData = (url, handler) => {
       throw new Error("Something is wrong with the data");
     })
     .then(handler);
+};
+
+const createList = data => {
+  if (data.Response == "False") {
+    console.log("falsefalse");
+    listItemError = document.createElement("div");
+    listItemError.classList.add("errorDiv");
+    listItem.appendChild(listItemError);
+    listItemError.innerText = "There are no results. Please try again";
+  } else {
+    data.Search.map(movie => {
+      const id = movie.imdbID;
+      const urlMovie =
+        "http://www.omdbapi.com/?apikey=f4f6588c&i=" + id + "&plot=full";
+      requestData(urlMovie, showDetails);
+      var c = listItem.getElementsByTagName("ul");
+      console.log(c.length);
+      return listItem;
+    }).forEach(item => {
+      list.appendChild(item);
+    });
+  }
 };
 
 const submitApp = function() {
